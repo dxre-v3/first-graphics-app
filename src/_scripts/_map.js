e86b3fef345784558666623cbf32e76ee735f88d
dxre-v2 <allenavriana1816@gmail.com>
2019-10-02 09:59:50 -0500

    Added mini map

--- src/_scripts/_map.js
+++ src/_scripts/_map.js
@@ -13,3 +13,10 @@ homicides.forEach(function(obj){
 		.addTo(map)
 		.bindTooltip(obj.first_name + " " + obj.last_name, {permanent:true});
 })
+
+
+var sat2 = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA', {
+    maxZoom: 8
+    });
+var mini = new L.Control.MiniMap(sat2);
+mini.addTo(map);
